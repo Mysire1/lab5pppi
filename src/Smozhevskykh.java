b1963849cbf26c58306fbaaa39cde764d6ee0b99
Kostyantyn Smozhevskykh <lee.ross2005@gmail.com>
2024-10-31 12:02:23 +0200

    Kostya

---
+++ src/Smozhevskykh.java
@@ -0,0 +1,15 @@
+public class Smozhevskykh implements leadclass {
+    @Override
+    public void printgrade() {
+
+    }
+
+    @Override
+    public void givearseniybanan() {
+
+    }
+
+    public void oleksiiotrimyiebananvidkosti() {
+        System.out.println("Oleksii otrimyie banan vid Kosti");
+    }
+}
---
+++ src/leadclass.java
@@ -0,0 +1,7 @@
+public interface leadclass {
+    void printgrade();
+
+    void givearseniybanan();
+
+    void oleksiiotrimyiebananvidkosti();
+}
